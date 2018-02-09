8616d64ad7d3b997ce9fa2eb0e8547834ef3598e
Brian He <brianhe@zipcoders-MacBook-Pro-56.local>
2018-02-09 09:23:54 -0500

    Person Handler

--- src/main/java/com/zipcodewilmington/PersonHandler.java
+++ src/main/java/com/zipcodewilmington/PersonHandler.java
@@ -1,8 +1,5 @@
 package com.zipcodewilmington;
 
-/**
- * Created by leon on 1/24/18.
- */
 public class PersonHandler {
     private final Person[] personArray;
 
@@ -12,49 +9,27 @@ public class PersonHandler {
 
     public String whileLoop() {
         String result = "";
-        // assume there is a `counter`
-        // while `counter` is less than length of array
-            // begin loop
-
-                // use `counter` to identify the `current Person` in the array
-                // get `string Representation` of `currentPerson`
-                // append `stringRepresentation` to `result` variable
-
-            // end loop
+        int counter = 0;
+        while(counter < personArray.length) {
+            result += this.personArray[counter];
+            counter++;
+        }
         return result;
     }
-
-
 
     public String forLoop() {
         String result = "";
-        // identify initial value
-        // identify terminal condition
-        // identify increment
-
-        // use the above clauses to declare for-loop signature
-            // begin loop
-                // use `counter` to identify the `current Person` in the array
-                // get `string Representation` of `currentPerson`
-                // append `stringRepresentation` to `result` variable
-            // end loop
-
+        for(int i = 0; i < personArray.length; i++) {
+            result += this.personArray[i];
+        }
         return result;
     }
 
-
-
     public String forEachLoop() {
         String result = "";
-        // identify array's type
-        // identify array's variable-name
-
-        // use the above discoveries to declare for-each-loop signature
-            // begin loop
-                // get `string Representation` of `currentPerson`
-                // append `stringRepresentation` to `result` variable
-            // end loop
-
+        for(Person person : personArray) {
+            result += person;
+        }
         return result;
     }
 
