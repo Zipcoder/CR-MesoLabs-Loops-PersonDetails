d91d2bad468110cb96a784f944b3b571ab78c0a3
Katrice Williams-Dredden <katricewilliams-dredden@zipcoders-MacBook-Pro.local>
2018-02-11 17:03:55 -0500

    done

--- src/main/java/com/zipcodewilmington/PersonHandler.java
+++ src/main/java/com/zipcodewilmington/PersonHandler.java
@@ -1,5 +1,7 @@
 package com.zipcodewilmington;
 
+import java.util.Arrays;
+
 /**
  * Created by leon on 1/24/18.
  */
@@ -13,7 +15,23 @@ public class PersonHandler {
     public String whileLoop() {
         String result = "";
         // assume there is a `counter`
+        int counter = 0;
         // while `counter` is less than length of array
+        while(counter < personArray.length){
+
+            /*This 'Person' is a data type/class and currentPerson is the name, personArray is array and counter is
+            our index*/
+            Person currentPerson = personArray[counter];
+            currentPerson.toString();
+            result += currentPerson;
+            counter++;
+
+            // begin loop
+            // use `counter` to identify the `current Person` in the array
+            // get `string Representation` of `currentPerson`
+            // append `stringRepresentation` to `result` variable
+            // end loop
+        }
             // begin loop
 
                 // use `counter` to identify the `current Person` in the array
@@ -31,6 +49,11 @@ public class PersonHandler {
         // identify initial value
         // identify terminal condition
         // identify increment
+        for(int i = 0; i < personArray.length ; i++){
+            Person currentPerson = personArray[i];
+            currentPerson.toString();
+            result += currentPerson;
+        }
 
         // use the above clauses to declare for-loop signature
             // begin loop
@@ -48,6 +71,11 @@ public class PersonHandler {
         String result = "";
         // identify array's type
         // identify array's variable-name
+        for (Person placeholder : personArray){
+            Person currentPerson = placeholder;
+            currentPerson.toString();
+            result += currentPerson;
+        }
 
         // use the above discoveries to declare for-each-loop signature
             // begin loop
