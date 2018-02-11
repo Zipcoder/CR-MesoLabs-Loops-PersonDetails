3231efd8d0afb8d9617ae9f581c42c2ce76ad4d9
Kaitrina High <kaitrinahigh@zipcoders-MacBook-Pro-81.local>
2018-02-10 23:48:39 -0500

    removed comments, formatted, removed whitespace

--- src/main/java/com/zipcodewilmington/PersonHandler.java
+++ src/main/java/com/zipcodewilmington/PersonHandler.java
@@ -11,57 +11,32 @@ public class PersonHandler {
     }
 
     public String whileLoop() {
+
         String result = "";
-        // assume there is a `counter`
         int count = 0;
-        // while `counter` is less than length of array
         while (count < personArray.length) {
-            // begin loop
             Person currentPerson = personArray[count];
             result += currentPerson.toString();
             count++;
-
-            // begin loop
-            // use `counter` to identify the `current Person` in the array
-            // get `string Representation` of `currentPerson`
-            // append `stringRepresentation` to `result` variable
-            //end loop
         }
         return result;
     }
 
 
     public String forLoop() {
-        String result = "";
-        // identify initial value
-        // identify terminal condition
-        // identify increment
 
-        // use the above clauses to declare for-loop signature
-        // begin loop
-        // use `counter` to identify the `current Person` in the array
-        // get `string Representation` of `currentPerson`
-        // append `stringRepresentation` to `result` variable
-        // end loop
+        String result = "";
         for(int i=0; i<personArray.length; i++){
             Person currentPerson = personArray[i];
             result += currentPerson.toString();
         }
-
         return result;
     }
 
 
     public String forEachLoop() {
-        String result = "";
-        // identify array's type
-        // identify array's variable-name
 
-        // use the above discoveries to declare for-each-loop signature
-        // begin loop
-        // get `string Representation` of `currentPerson`
-        // append `stringRepresentation` to `result` variable
-        // end loop
+        String result = "";
         for(Person currentPerson : personArray){
             result += currentPerson.toString();
         }
