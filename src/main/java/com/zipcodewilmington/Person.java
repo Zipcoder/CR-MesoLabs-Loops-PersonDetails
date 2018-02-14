1a4d9904a842741e5f4de2bf15742a7b90dfc56c
Amy Gill <amygill@MacBook-Pro-2.local>
2018-02-14 18:02:36 -0500

    re-clarifying

--- src/main/java/com/zipcodewilmington/Person.java
+++ src/main/java/com/zipcodewilmington/Person.java
@@ -7,18 +7,33 @@ public class Person {
     private final String firstName;
     private final String lastName;
 
+    //our person constructor
     public Person(String firstName, String lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
     }
-
+    //our getter method that returns a String type of first name
     public String getFirstName() {
-        return firstName;
-    }
 
+        return firstName;
+
+    }
+    //our getter method that returns a String type of first name
     public String getLastName() {
+
         return lastName;
     }
+
+// Person brian = new Person("Brian", "He");
+
+/*since brian is an object of Person class, to use any method in the person class, start with the object name, then use
+the . plus method(). see below:
+
+
+Wondering if you need to put anything in the parentheses? Just look back at the original getter method!
+brian.getFirstName();
+
+*/
 
     @Override
     public String toString() {
