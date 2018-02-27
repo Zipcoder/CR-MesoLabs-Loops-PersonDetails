c623cfb1aa605174e6f3802cb48d778ac218a7b6
Kthomas <kthomas@github.com>
2018-02-27 13:58:11 -0500

    Clean

--- src/main/java/com/zipcodewilmington/PersonHandler.java
+++ src/main/java/com/zipcodewilmington/PersonHandler.java
@@ -5,13 +5,10 @@ package com.zipcodewilmington;
  */
 public class PersonHandler {
     private final Person[] personArray;
-
     public PersonHandler(Person[] personArray) {
         this.personArray = personArray;
     }
-
     public String whileLoop() {
-
         String name = "";
         int counter = 0;
         while (counter < personArray.length) {
@@ -20,40 +17,29 @@ public class PersonHandler {
             name += currentDude;
             counter++;
         }
-
         return name;
     }
 
-
     public String forLoop() {
-
         String name = "";
         for (int i = 0; i < personArray.length; i++) {
             Person currentDude = personArray[i];
             currentDude.toString();
             name += currentDude;
         }
-
         return name;
     }
 
-
-
-
         public String forEachLoop() {
-
             String name = "";
             for (Person firstLast : personArray) {
                 firstLast.toString();
                 name += firstLast;
             }
-
             return name;
         }
 
-
         public Person[] getPersonArray () {
-
             return personArray;
         }
     }
