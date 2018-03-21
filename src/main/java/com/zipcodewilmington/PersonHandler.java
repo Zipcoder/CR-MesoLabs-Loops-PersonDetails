424af03ebef52d0263e24fc4eb27818a6d3b07d7
Dominique Clarke <dominiquedclarke@gmail.com>
2018-03-21 12:58:06 -0400

    first commit

--- src/main/java/com/zipcodewilmington/PersonHandler.java
+++ src/main/java/com/zipcodewilmington/PersonHandler.java
@@ -12,15 +12,14 @@ public class PersonHandler {
 
     public String whileLoop() {
         String result = "";
-        // assume there is a `counter`
-        // while `counter` is less than length of array
-            // begin loop
 
-                // use `counter` to identify the `current Person` in the array
-                // get `string Representation` of `currentPerson`
-                // append `stringRepresentation` to `result` variable
+        int i = 0;
+        while (i < personArray.length)
+        {
+            result += personArray[i].toString();
+            i++;
+        }
 
-            // end loop
         return result;
     }
 
@@ -28,16 +27,11 @@ public class PersonHandler {
 
     public String forLoop() {
         String result = "";
-        // identify initial value
-        // identify terminal condition
-        // identify increment
 
-        // use the above clauses to declare for-loop signature
-            // begin loop
-                // use `counter` to identify the `current Person` in the array
-                // get `string Representation` of `currentPerson`
-                // append `stringRepresentation` to `result` variable
-            // end loop
+        for (int i = 0; i < personArray.length; i++)
+        {
+            result += personArray[i].toString();
+        }
 
         return result;
     }
@@ -46,14 +40,10 @@ public class PersonHandler {
 
     public String forEachLoop() {
         String result = "";
-        // identify array's type
-        // identify array's variable-name
 
-        // use the above discoveries to declare for-each-loop signature
-            // begin loop
-                // get `string Representation` of `currentPerson`
-                // append `stringRepresentation` to `result` variable
-            // end loop
+        for (Person person : personArray) {
+            result += person.toString();
+        }
 
         return result;
     }
