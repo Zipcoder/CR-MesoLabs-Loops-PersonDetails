3bdc1f9d32244a845ca3c7e02f668d9c3611e026
Brandon Chambers <brandonmchambers36@gmail.com>
2019-10-22 13:58:59 -0400

    Complete loop (while & for)

--- src/main/java/com/zipcodewilmington/PersonHandler.java
+++ src/main/java/com/zipcodewilmington/PersonHandler.java
@@ -17,21 +17,21 @@ public class PersonHandler {
 
     public String whileLoop() {
 
-        while( i < personArray.length)
+        while(  i < personArray.length) {
 
             result += personArray[i];// Counter identifies each array index value and appends it to result
             i++;
 
-        // create a `counter`
-        // while `counter` is less than length of array
+            // create a `counter`
+            // while `counter` is less than length of array
             // begin loop
 
-                // use `counter` to identify the `current Person` in the array
-                // get `string Representation` of `currentPerson`
-                // append `stringRepresentation` to `result` variable
+            // use `counter` to identify the `current Person` in the array
+            // get `string Representation` of `currentPerson`
+            // append `stringRepresentation` to `result` variable
 
             // end loop
-
+        }
         return result;// returns the array values in a string
     }
 
@@ -39,9 +39,14 @@ public class PersonHandler {
 
     public String forLoop() {
         String result = "";
+
         // identify initial value
         // identify terminal condition
         // identify increment
+
+        for(int counter = 0; counter < personArray.length; counter++){
+            result += personArray[counter];
+        }
 
         // use the above clauses to declare for-loop signature
             // begin loop
@@ -59,7 +64,10 @@ public class PersonHandler {
         String result = "";
         // identify array's type
         // identify array's variable-name
+    for(int i = 0; i < personArray.length; i++){
+            result += personArray[i];
 
+    }
         // use the above discoveries to declare for-each-loop signature
             // begin loop
                 // get `string Representation` of `currentPerson`
