b50acf3af4a5db0e80f2950b8ec67753c763d703
MannyMonocle <eetumadu@gmail.com>
2019-10-22 21:58:45 -0400

    forLoop and whileLoop both work

--- src/main/java/com/zipcodewilmington/PersonHandler.java
+++ src/main/java/com/zipcodewilmington/PersonHandler.java
@@ -13,12 +13,18 @@ public class PersonHandler {
     public String whileLoop() {
         String result = "";
         // create a `counter`
+        int i = 0;
         // while `counter` is less than length of array
+        while (i < personArray.length) {
             // begin loop
-                // use `counter` to identify the `current Person` in the array
-                // get `string Representation` of `currentPerson`
-                // append `stringRepresentation` to `result` variable
-                // end loop
+            // use `counter` to identify the `current Person` in the array
+            Person currentPerson = personArray[i];
+            // get `string Representation` of `currentPerson`
+            // append `stringRepresentation` to `result` variable
+            result += currentPerson.toString();
+            // end loop
+            i++;
+        }
         return result;
     }
 
