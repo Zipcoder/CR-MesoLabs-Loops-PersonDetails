6c8162563fee6bd5b3c9f7127d73ce976f25ec9b
sitarameez <sitarablossom@gmail.com>
2021-06-29 12:08:35 -0400

    completed while and for loop

--- src/main/java/com/zipcodewilmington/PersonHandler.java
+++ src/main/java/com/zipcodewilmington/PersonHandler.java
@@ -11,17 +11,27 @@ public class PersonHandler {
     }
 
     public String whileLoop() {
-        String result = "";
+        //String result = "";
         // create a `counter`
         // while `counter` is less than length of array
             // begin loop
+        int counter=0;
+
+        StringBuilder array = new StringBuilder();
+
+            while(counter< personArray.length){
+//
+                array.append(personArray[counter]);
+                counter++;
+            //array.append(System.lineSeparator());
+        }
 
                 // use `counter` to identify the `current Person` in the array
                 // get `string Representation` of `currentPerson`
                 // append `stringRepresentation` to `result` variable
 
             // end loop
-        return result;
+        return array.toString();
     }
 
 
@@ -31,15 +41,19 @@ public class PersonHandler {
         // identify initial value
         // identify terminal condition
         // identify increment
-
+        //int counter=0;
+        StringBuilder array = new StringBuilder();
+        for(int counter=0;counter< personArray.length;counter++)
+        {
+            array.append(personArray[counter]);
+        }
+        return array.toString();
         // use the above clauses to declare for-loop signature
             // begin loop
                 // use `counter` to identify the `current Person` in the array
                 // get `string Representation` of `currentPerson`
                 // append `stringRepresentation` to `result` variable
             // end loop
-
-        return result;
     }
 
 
@@ -48,14 +62,16 @@ public class PersonHandler {
         String result = "";
         // identify array's type
         // identify array's variable-name
-
+int counter=0;
+StringBuilder array = new StringBuilder();
+for()
         // use the above discoveries to declare for-each-loop signature
             // begin loop
                 // get `string Representation` of `currentPerson`
                 // append `stringRepresentation` to `result` variable
             // end loop
 
-        return result;
+
     }
 
 
